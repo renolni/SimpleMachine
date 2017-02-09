e2b7a942af08b354d51054748f678ee9ce4695cf
renolni <nicolas.renollet@ecolab.com>
2017-02-09 15:53:55 +0100

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,1 +1,7 @@
-# SimpleMachine
+# Install a simple Virtual Machine
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fgithub.com%2Frenolni%2FSimpleMachine%2Fblob%2Fmaster%2FsimpleMachine.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+
+This template is a test from NRE
